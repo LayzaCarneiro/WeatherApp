e42baba9196d7031b7b3bd07743dbc73fba6bc56
Layza Carneiro <layza.mrcarneiro@gmail.com>
2025-11-30 21:03:30 -0300

    implement alamofire to make HTTP Get request

--- WeatherApp/WeatherService.swift
+++ WeatherApp/WeatherService.swift
@@ -6,6 +6,11 @@
 //
 
 import Foundation
+import Alamofire
+
+enum WeatherEndpoint {
+    static let base = "https://api.openweathermap.org/data/3.0/onecall"
+}
 
 struct City {
     let lat: String
@@ -14,47 +19,43 @@ struct City {
 }
 
 class WeatherService {
-    
-    private let baseURL: String = "https://api.openweathermap.org/data/3.0/onecall"
-    private let session = URLSession.shared
-    
+        
     private let apiKey: String = {
         guard let url = Bundle.main.url(forResource: "APIKey", withExtension: "plist"),
               let data = try? Data(contentsOf: url),
               let plist = try? PropertyListSerialization.propertyList(from: data, format: nil) as? [String: Any],
               let key = plist["WeatherAPIKey"] as? String else {
-            fatalError("⚠️ API Key not found in Secrets.plist")
+            fatalError("API key not found")
         }
         return key
     }()
     
     func fetchData(city: City, _ completion: @escaping (ForecastResponse?) -> Void) {
-        let urlString = "\(baseURL)?lat=\(city.lat)&lon=\(city.lon)&appid=\(apiKey)&units=metric"
         
-        guard let url = URL(string: urlString) else { return }
+        let parameters: [String: String] = [
+            "lat": city.lat,
+            "lon": city.lon,
+            "appid": apiKey,
+            "units": "metric"
+        ]
         
-        let task = session.dataTask(with: url) { data, response, error in
+        AF.request(
+            WeatherEndpoint.base,
+            method: .get,
+            parameters: parameters
+        )
+        .validate()
+        .responseDecodable(of: ForecastResponse.self) { response in
             
-            guard let data else {
-                completion(nil)
-                return
-            }
-            
-            do {
-                if let jsonString = String(data: data, encoding: .utf8) {
-                    print("JSON response:\n\(jsonString)")
-                }
-
-                let forecastResponse = try JSONDecoder().decode(ForecastResponse.self, from: data)
-                completion(forecastResponse)
-            } catch {
-                print(error)
+            switch response.result {
+            case .success(let forecast):
+                completion(forecast)
+                
+            case .failure(let error):
+                print("Error: \(error.localizedDescription)")
                 completion(nil)
             }
-
         }
-        
-        task.resume()
     }
     
 }
